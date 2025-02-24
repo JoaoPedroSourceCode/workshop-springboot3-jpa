db540792b509174876016e3e8f142267079af33e
Joao Pedro <joaoapedroalvesdasilva26@gmail.com>
2025-02-24 09:36:08 -0300

    User entity and resource

---
+++ src/main/java/com/example/project/entities/User.java
@@ -0,0 +1,68 @@
+package com.example.project.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Integer id;
+    private String name;
+    private String phone;
+    private String password;
+
+    public User() {
+    }
+
+    public User(Integer id, String name, String phone, String password) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+        this.password = password;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
